f79dc988027d79cf10053337b6e830e0ec83122c
chloebocker <chloebocker@gmail.com>
2018-05-18 14:31:08 -0700

    Committing another console app

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 2.3m;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+        }
+    }
+
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
