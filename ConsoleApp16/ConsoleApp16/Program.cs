875253473c232d4f62f365f8443f0e18268c6adb
chloebocker <chloebocker@gmail.com>
2018-05-18 11:39:25 -0700

    Committing another console app

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week: ");
+                string day = Console.ReadLine();
+                DayOfWeek parsedDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), day);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }         
+        }
+    }
+
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
