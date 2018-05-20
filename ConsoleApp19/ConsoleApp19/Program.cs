cbe39c52ddb3ead69b85dc0330f1b30c5d87b5d5
chloebocker <chloebocker@gmail.com>
2018-05-20 11:52:02 -0700

    Committing another console app

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string path = "C:\\Users\\Chloe\\source\\repos\\MyConsoleApps\\ConsoleApp19\\Logs\\log.txt";
+            File.WriteAllText(path, Console.ReadLine());
+            Console.WriteLine(File.ReadAllText(path));
+            Console.ReadLine();
+        }
+    }
+}
