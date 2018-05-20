7f992797abdc7086677e470eaff79c89c9e84092
chloebocker <chloebocker@gmail.com>
2018-05-20 13:43:53 -0700

    Committing another console app

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Current date and time is: " + DateTime.Now);
+            Console.ReadLine();
+            Console.WriteLine("Enter a number: ");
+            string hours = Console.ReadLine();
+            Double hoursToAdd = Convert.ToDouble(hours);
+            DateTime newDateTime = DateTime.Now.AddHours(hoursToAdd);
+
+            Console.WriteLine("The exact time it will be in " + hours + " hours is: " + newDateTime.TimeOfDay);
+            Console.ReadLine();
+        }
+    }
+}
